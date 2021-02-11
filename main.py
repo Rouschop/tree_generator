021c5b5e5c34e7f3b4491c176bdc56658b1bfccc
Jordy Rouschop <j.rouschop@outlook.com>
2021-02-11 15:40:09 -0500

    Added different tree different colour

--- main.py
+++ main.py
@@ -16,14 +16,21 @@ space = '    '
 branch = '│   '
 tee = '├── '
 last = '└── '
+colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+color = 0
 
 
-def tree(path: Path, prefix='', is_dir=None):
-    files = list(path.iterdir())  # Iterate over the files and dirs in directory.
+def tree(dir_path: Path, prefix='', is_dir=None):
+    global tree_symbols
+    global color
+    files = list(dir_path.iterdir())  # Iterate over the files and dirs in directory.
     if not is_dir:
         tree_symbols = [tee] * (len(files) - 1) + [last]
     if is_dir:
-        tree_symbols = [colored(tee, 'red')] * (len(files) - 1) + [colored(last, 'red')]
+        color += 1
+        if color == 6:
+            color = 0
+        tree_symbols = [colored(tee, colors[color])] * (len(files) - 1) + [colored(last, colors[color])]
     for tree_symbol, file in zip(tree_symbols, files):
         if not is_dir:
             yield prefix + tree_symbol + file.name
@@ -32,20 +39,14 @@ def tree(path: Path, prefix='', is_dir=None):
         if file.is_dir():  # Also print out the contents of directories with correct symbols
             if tree_symbol == tee:
                 symbol = colored(branch, 'red')
-                tree_symbol = colored(tree_symbol, 'red')
             else:  # Without tree looks like: │   │   │   │   └── origin
                 symbol = space
             yield from tree(file, prefix=prefix + symbol, is_dir=True)
 
 
-
-
-
-
-
 if path:
-    for line in tree(path=pathlib.Path(__file__).parent.absolute() / path):
+    for line in tree(dir_path=pathlib.Path(__file__).parent.absolute() / path):
         print(line)
 else:
-    for line in tree(path=pathlib.Path(__file__).parent.absolute()):
+    for line in tree(dir_path=pathlib.Path(__file__).parent.absolute()):
         print(line)
