62a5cd66cbd6aa66115f0e5bdbfd6f28e7a2804e
Jordy Rouschop <j.rouschop@outlook.com>
2021-02-11 12:39:47 -0500

    Added tree coloring and filename colors

--- main.py
+++ main.py
@@ -18,19 +18,27 @@ tee = '├── '
 last = '└── '
 
 
-def tree(path: Path, prefix=''):
+def tree(path: Path, prefix='', is_dir=None):
     files = list(path.iterdir())  # Iterate over the files and dirs in directory.
-    # print(files)
-    tree_symbols = [tee] * (len(files) - 1) + [last]
+    if not is_dir:
+        tree_symbols = [tee] * (len(files) - 1) + [last]
+    if is_dir:
+        tree_symbols = [colored(tee, 'red')] * (len(files) - 1) + [colored(last, 'red')]
     for tree_symbol, file in zip(tree_symbols, files):
         # print(tree_symbol, file)
-        yield prefix + tree_symbol + file.name
+        yield prefix + tree_symbol + colored(file.name, 'blue')
         if file.is_dir():  # Also print out the contents of directories with correct symbols
             if tree_symbol == tee:
                 symbol = colored(branch, 'red')
+                tree_symbol = colored(tree_symbol, 'red')
             else:  # Without tree looks like: │   │   │   │   └── origin
                 symbol = space
-            yield from tree(file, prefix=prefix + symbol)
+            yield from tree(file, prefix=prefix + symbol, is_dir=True)
+
+
+
+
+
 
 
 if path:
